91986f9bcd0ac33e3f14c725d063092f0b7973e9
Jackson Newman <72842312+JNewman-cell@users.noreply.github.com>
2021-08-28 18:40:16 -0700

    Create stockCompiler.py

---
+++ stockCompiler.py
@@ -0,0 +1,19 @@
+import csv
+
+temp = []
+EveryStockInSP = []
+
+with open('S&P500HistoricalComponents.csv', 'r' ) as fr:
+	reader = csv.reader(fr)
+	for row in reader:
+		temp = row[1].rsplit(",")
+		for ticker in temp:
+			if EveryStockInSP.count(ticker) < 1:
+				print(ticker)
+				EveryStockInSP.append(ticker)
+
+EveryStockInSP.pop(0)
+
+with open('S&PList.txt', 'w') as nf:
+	for stock in EveryStockInSP:
+		nf.write(stock + "\n")
