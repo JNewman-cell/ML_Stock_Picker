7537542582b25fc2a7c58abe148098c65e6343d6
Jackson Newman <72842312+JNewman-cell@users.noreply.github.com>
2021-08-28 23:09:20 -0700

    Update extraStockRemoval.py

--- extraStockRemoval.py
+++ extraStockRemoval.py
@@ -1,4 +1,14 @@
-import csv
+import shutil
 import os
 
+filenameEnding = ".us.txt"
+
+original = "/Users/jacknewman/Documents/GitHub/ML_Stock_Picker/stockPriceData/"
+new = "/Users/jacknewman/Documents/GitHub/ML_Stock_Picker/S&PHistoricalStockPrice"
+
+with open('S&PList.txt') as file:
+	for line in file:
+		path = line.rstrip().lower() + filenameEnding
+		if os.path.exists(original+path):
+			shutil.move(original+path, new+path)
 
