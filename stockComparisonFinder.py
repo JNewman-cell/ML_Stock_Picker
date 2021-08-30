ea6a6f43f0964d9bf7ac88cb3735b41c046e51e9
Jackson Newman <72842312+JNewman-cell@users.noreply.github.com>
2021-08-30 14:05:28 -0700

    Update stockComparisonFinder.py

--- stockComparisonFinder.py
+++ stockComparisonFinder.py
@@ -1,19 +1,22 @@
-filename1 = "SPList1996 copy.txt"
-filename2 = "SPList1997 copy.txt"
+startingYear = 1996
+timeIntervalInYears = 5
 
-File1 = open(filename1,"r")
-File2 = open(filename2,"r")
-Dict1 = File1.readlines()
-Dict2 = File2.readlines()
+while startingYear + timeIntervalInYears < 2020:
+    filename1 = "SPList" + str(startingYear) + "copy.txt"
+    filename2 = "SPList" + str(startingYear+timeIntervalInYears) + "copy.txt"
 
-year1 = filename1[6:10]
-year2 = filename2[6:10]
+    File1 = open(filename1,"r")
+    File2 = open(filename2,"r")
+    Dict1 = File1.readlines()
+    Dict2 = File2.readlines()
 
-for line in Dict1:
-    if Dict2.count(line) < 1:
-        Dict1.remove(line)
-        print(line)
+    for line in Dict1:
+        if Dict2.count(line) < 1:
+            Dict1.remove(line)
+            print(line)
 
-with open(year1 + "-" + year2 + 'commonStocks.txt', 'w') as nf:
-	for stock in Dict1:
-		nf.write(stock)
+    with open(str(startingYear) + "-" + str(startingYear+timeIntervalInYears) + 'commonStocks.txt', 'w') as nf:
+        for stock in Dict1:
+            nf.write(stock)
+
+    startingYear+=timeIntervalInYears
