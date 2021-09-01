7d8cf75a1e849d4aa64b0f3995279b995030d846
JNewman-cell <72842312+JNewman-cell@users.noreply.github.com>
2021-09-01 00:09:21 -0700

    Update stocksOutperformed.py

--- stocksOutperformed.py
+++ stocksOutperformed.py
@@ -1,34 +1,39 @@
 import csv
 
-startYear = 1998
-endYear = 2003
-timeInterval = 5
+def FindStocksThatOutperformed(start, end, interval):
+    startYear = start
+    endYear = end
+    timeInterval = interval
 
-from stockComparisonFinder import FindContinuousStocks
+    from stockComparisonFinder import FindContinuousStocks
 
-FindContinuousStocks(startYear, endYear, timeInterval)
+    FindContinuousStocks(startYear, endYear, timeInterval)
 
-SPfile = open(str(startYear)+"-"+str(endYear)+".txt", 'r')
-dict = SPfile.readlines()
-outperformers = []
-for line in dict:
-    foundStart = False
-    foundEnd = False
-    startingPrice = 0
-    endingPrice = 0
-    startOfYear = str(startYear)+"-01"
-    endOfYear = str(endYear)+"-01"
-    with open(line.lower()+".csv", 'r') as fr:
-        reader = csv.reader(fr)
-        for row in reader:
-            if not foundStart:
-                if startOfYear in row[0]:
-                    startingPrice = float(row[2])
-                    foundStart = True
-        for row in reader:
-            if not foundEnd:
-                if endOfYear in row[0]:
-                    endingPrice = float(row[2])
-                    foundEnd = True
-    if endingPrice/startingPrice > 1.1:
-        outperformers.append(line)
+    SPfile = open(str(startYear)+"-"+str(endYear)+".txt", 'r')
+    dict = SPfile.readlines()
+    outperformers = []
+    for line in dict:
+        foundStart = False
+        foundEnd = False
+        startingPrice = 0
+        endingPrice = 0
+        startOfYear = str(startYear)+"-01"
+        endOfYear = str(endYear)+"-01"
+        with open(line.lower()+".csv", 'r') as fr:
+            reader = csv.reader(fr)
+            for row in reader:
+                if not foundStart:
+                    if startOfYear in row[0]:
+                        startingPrice = float(row[2])
+                        foundStart = True
+            for row in reader:
+                if not foundEnd:
+                    if endOfYear in row[0]:
+                        endingPrice = float(row[2])
+                        foundEnd = True
+        if endingPrice/startingPrice > 1.1:
+            outperformers.append(line)
+
+    with open('SPOutperformers' + startYear + '-' + endYear + '.txt', 'w') as nf:
+            for stock in outperformers:
+                nf.write(stock + '\n')
