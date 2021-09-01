a48dda2f37f841f76b80dd0bbab0ed3f8d990da2
JNewman-cell <72842312+JNewman-cell@users.noreply.github.com>
2021-08-31 23:45:29 -0700

    Update stocksOutperformed.py

--- stocksOutperformed.py
+++ stocksOutperformed.py
@@ -1,3 +1,5 @@
+import csv
+
 startYear = 1998
 endYear = 2003
 timeInterval = 5
@@ -8,10 +10,25 @@ FindContinuousStocks(startYear, endYear, timeInterval)
 
 SPfile = open(str(startYear)+"-"+str(endYear)+".txt", 'r')
 dict = SPfile.readlines()
+outperformers = []
 for line in dict:
-    found = False
+    foundStart = False
+    foundEnd = False
+    startingPrice = 0
+    endingPrice = 0
     startOfYear = str(startYear)+"-01"
-    stockFile = open(line+".us.txt", 'r')
-    dict2 = stockFile.readlines()
-        if not found:
-            if startOfYear 
+    endOfYear = str(endYear)+"-01"
+    with open(line.lower()+".csv", 'r') as fr:
+        reader = csv.reader(fr)
+        for row in reader:
+            if not foundStart:
+                if startOfYear in row[0]:
+                    startingPrice = float(row[2])
+                    foundStart = True
+        for row in reader:
+            if not foundEnd:
+                if endOfYear in row[0]:
+                    endingPrice = float(row[2])
+                    foundEnd = True
+    if endingPrice/startingPrice > 1.1:
+        outperformers.append(line)
